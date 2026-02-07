8723aaf3e9dc90756bf62cea88542fa14ef85869
Thauã Borges <thaua.borges-ext@merkle.com>
2026-02-07 20:26:14 -0300

    feat: add authentication endpoint for user sessions

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,9 @@
-import { register } from './controllers/register';
 import type { FastifyInstance } from 'fastify';
+import { register } from './controllers/register';
+import { authenticate } from './controllers/authenticate';
 
 export async function appRoutes(app: FastifyInstance) {
 	// Register User
 	app.post('/users', register);
+	app.post('/sessions', authenticate);
 }
